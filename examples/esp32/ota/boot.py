7f4ad4bbe4912f60ddbb89ea8c517365ea281f24
brunoricardojava <brunoricardojava@gmail.com>
2023-12-12 15:58:08 -0300

    Add version file.

--- examples/esp32/ota/boot.py
+++ examples/esp32/ota/boot.py
@@ -1,6 +1,18 @@
 # boot.py -- run on boot-up
-from ota import OTAUpdater
+import network
+from time import sleep_ms
 from secret import ssid, password
 
 
-firmware_url = "https://github.com/brunoricardojava/micropython/tree/master/examples/esp32/ota"
+station = network.WLAN(network.STA_IF)
+station.active(True)
+
+station.connect(ssid, password)
+
+print("Connecting", end="")
+
+while not station.isconnected():
+    print(".", end="")
+    sleep_ms(500)
+
+print(f"Conected. Network config: {station.ifconfig()}")
--- examples/esp32/ota/lib/ota_example.py
+++ examples/esp32/ota/lib/ota_example.py
@@ -6,22 +6,43 @@ import urequests as requests
 class OTAUpdater:
     def __init__(self, repo_url: str, filenames: list) -> None:
         self._version_file_name = "version.json"
-        self._repo_url = repo_url.strip('/')
+        self._repo_url = self._repo_url_parser(repo_url)
         self._filenames = filenames
-        self._version_url = self._version_url_parser(self._repo_url, self._version_file_name)
-        self._current_version = self._get_current_version(self._version_file_name)
+        self._version_url = self._version_url_parser()
+        self._current_version = self._get_current_version()
+        
+    def _repo_url_parser(self, repo_url) -> str:
+        repo_url = self._change_domain_url(repo_url)
+        repo_url = self._remove_tree_url(repo_url)
+        return repo_url
+    
+    @staticmethod
+    def _change_domain_url(repo_url: str) -> str:
+        repo_url = repo_url.strip("/")
+        domain_to_replace = "https://github.com/"
+        domain_to_use = "https://raw.githubusercontent.com/"
+        return repo_url.replace(domain_to_replace, domain_to_use)
+    
+    @staticmethod
+    def _remove_tree_url(repo_url: str) -> str:
+        term_to_remove = "/tree"
+        return repo_url.replace(term_to_remove, "")
 
     def _version_url_parser(self) -> str:
         return self._repo_url + "/" + self._version_file_name
 
     def _get_current_version(self) -> str:
         if self._version_file_name in os.listdir():
-            with open(self._version_file_name) as version_file:
-                current_version = json.load(version_file)
+            with open(self._version_file_name, "r+") as version_file:
+                try:
+                    version_json = json.load(version_file)
+                    current_version = version_json.get("version")
+                except json.JSONDecodeError:
+                    print(f"Erro ao abrir o arquivo {self._version_file_name}")
         else:
             current_version = "0"
             with open(self._version_file_name, "w") as version_file:
-                json.dump({"version": current_version})
+                json.dump({"version": current_version}, version_file)
 
         return current_version
 
